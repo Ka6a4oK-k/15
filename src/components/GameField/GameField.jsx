5a19f1b29339f4812197e523e13ff0445fcc7d11
Ka6a4oK-k <72969414+Ka6a4oK-k@users.noreply.github.com>
2023-03-27 10:39:27 +0300

    tile move added

--- src/components/GameField/GameField.jsx
+++ src/components/GameField/GameField.jsx
@@ -16,19 +16,59 @@ export default function GameField({ cols, rows }) {
           num: (y * numOfCols + x + 1),
           x: x + 1,
           y: y + 1,
+          empty: (y===numOfRows-1 && x===numOfCols-1)
         })
       }
     }
     setTilesArray([...arr])
   }, [])
 
+  function moveTile(tile){
+    const emptyTile = tilesArray.find((element) => element.empty)
+    // console.log(tile);
+    // console.log(emptyTile);
+    if((tile.x - emptyTile.x === 1) && (tile.y - emptyTile.y === 0)) {
+      const tempTileX = emptyTile.x
+      emptyTile.x = tile.x
+      tile.x = tempTileX
+      // console.log('move left');
+      setTilesArray([...tilesArray])
+      return
+    }
+    if((tile.x - emptyTile.x === -1) && (tile.y - emptyTile.y === 0)) {
+      const tempTileX = emptyTile.x
+      emptyTile.x = tile.x
+      tile.x = tempTileX
+      // console.log('move right');
+      setTilesArray([...tilesArray])
+      return
+    }
+    if((tile.x - emptyTile.x === 0) && (tile.y - emptyTile.y === 1)) {
+      const tempTileY = emptyTile.y
+      emptyTile.y = tile.y
+      tile.y = tempTileY
+      // console.log('move up');
+      setTilesArray([...tilesArray])
+      return
+    }
+    if((tile.x - emptyTile.x === 0) && (tile.y - emptyTile.y === -1)) {
+      const tempTileY = emptyTile.y
+      emptyTile.y = tile.y
+      tile.y = tempTileY
+      // console.log('move down');
+      setTilesArray([...tilesArray])
+      return
+    }
+
+  }
+
   return (
     <div className='game-field__wrapper'>
-      <div className='game-field' onClick={() => { console.log(tilesArray); }}>
+      <div className='game-field'>
         {
           tilesArray.map((element, index) => {
             if (!(tilesArray.length === index + 1)) {
-              return <Tile element={element} />
+              return <Tile key={index} element={element} onClick={() => moveTile(element)}/>
             } 
             return
           })
--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Tile.css'
 
-export default function Tile({element}) {
+export default function Tile({element, onClick}) {
 
 
 
   return (
-    <div className='tile' style={{top: `calc(${25*(element.y-1)}% + 1px)`, left: `calc(${25*(element.x-1)}% + 1px)`}}>
-        {element.num}
+    <div
+      className='tile'
+      onClick={onClick}
+      style={{ top: `calc(${25 * (element.y - 1)}% + 1px)`, left: `calc(${25 * (element.x - 1)}% + 1px)` }}
+    >
+      {element.num}
     </div>
   )
 }
