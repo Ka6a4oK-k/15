249cc976e5b0582c8a8afdbdab48bd635c5ba664
Ka6a4oK-k <72969414+Ka6a4oK-k@users.noreply.github.com>
2023-04-08 13:08:38 +0300

    added styles for color themes

--- src/components/ThemePicker/ThemePicker.jsx
+++ src/components/ThemePicker/ThemePicker.jsx
@@ -4,7 +4,16 @@ import './ThemePicker.css'
 export default function ThemePicker({close, active}) {
   return (
     <div className={active ? 'theme-picker theme-picker-active' : 'theme-picker'}>
-        
+        <form action="" className="theme-picker-form">
+          <input type="radio" name="theme" id="theme-1" />
+          <label htmlFor="theme-1">Theme-1</label>
+          <input type="radio" name="theme" id="theme-2" />
+          <label htmlFor="theme-2">Theme-2</label>
+          <input type="radio" name="theme" id="theme-3" />
+          <label htmlFor="theme-3">Theme-3</label>
+          <input type="radio" name="theme" id="theme-4" />
+          <label htmlFor="theme-4">Theme-4</label>
+        </form>
     </div>
   )
 }
